661f5ae852fb777b16d70cb4d6de502db1002ba4
fuyicai <1400330380@qq.com>
2020-10-13 09:45:23 +0800

    madd in main checkPlatfrom

--- src/main.js
+++ src/main.js
@@ -8,7 +8,15 @@ Vue.use(animated)
 import axios from 'axios'
 import baseURL from './utils/config'
 Vue.prototype.$img_url = baseURL.img_url;
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
+
 axios.defaults.baseURL = baseURL.base_url;
+
+if (!(/ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(navigator.userAgent.toLowerCase()))) {
+	  window.location.replace("https://yulangsat.com" + router.history.current.fullPath)
+}
+
+
 Vue.prototype.$H = axios
 
 import './plugins/vant'
